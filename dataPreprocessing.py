c56d14a56a96275a4b37f6828c813a051ce90d1a
KosmicKatXV <66205369+KosmicKatXV@users.noreply.github.com>
2024-05-30 16:08:44 +0200

    Added arguments
     *Added verbose and neighbour arguments
     *TODO fix "fixNaN" function

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -1,20 +1,30 @@
 import pandas as pd
 import os.path
+from sklearn.impute import KNNImputer
 
-def importData(*args):
-    if(len(args) > 1):
-        fname = args[1];
-    else:
-        fname = 'Data.xlsx'
+def importData(fname):
     if(os.path.isfile(fname)):
         return pd.read_excel(fname)
     else:
         raise Exception("No file was found or was invalid. Please check root folder and arguments.") 
     
-def showNaN(data):
-    print("Missing values:")
-    print( data.isna().sum().where(lambda x : x!=0.00).dropna().to_string())
-    
-    print("samples with missing data:")
-    nanSeriesRows = data.isna().sum(axis=1)
-    print(nanSeriesRows.groupby(nanSeriesRows).size().where(lambda x : x!=0.00).dropna().to_string())
+def showNaN(data,verbose):
+    print("Columns with missing values: %i" % data.isna().shape[1])
+    if(verbose):
+        print( data.isna().sum().where(lambda x : x!=0.00).dropna().to_string())
+    print("Rows with missing data: %i" % data.isna().shape[1])
+    if(verbose):
+        nanSeriesRows = data.isna().sum(axis=1)
+        print(nanSeriesRows.groupby(nanSeriesRows).size().where(lambda x : x!=0.00).dropna().to_string())
+
+def fixNaN(data,verbose,n):
+    nan_columns = data.isna().columns
+    nan_df= data.isna()
+    nan_array = nan_df.to_numpy()
+    if(verbose): print("Columns with missing data:")
+    if(verbose): print(nan_df.dtypes.to_string()) #We can see that missin values are either float or integers so we apply a fitting algorithm
+    imputer = KNNImputer(n_neighbors=2, weights="uniform")
+    imputer.set_output(transform='pandas')
+    nan_df = imputer.fit_transform(nan_array)
+    nan_df.columns = nan_columns
+    return data.fillna(nan_df)
--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import pandas as pd
 import dataPreprocessing as dp
+import sys
+import argparse
 
 def main():
-    df = dp.importData()
-    print(df.columns.to_list())
+
+    parser = argparse.ArgumentParser(
+                    prog='Progetto FIA Singolo 2024',
+                    description='Receives a dataset of patients of "neoplasia maligna" and it predicts if the next sample will be "Recidiva"',
+                    epilog='by Pablo Tores Rodriguez')
+    parser.add_argument('-f ',  '--filename',     type=str, default='Data.xlsx')           # positional argument
+    parser.add_argument('-n',   '--neighbours',   type=int, default=2)      # option that takes a value
+    parser.add_argument('-v',   '--verbose',      action='store_true')  # on/off flag
+    args = parser.parse_args()
+
+    print("importing dataset...")
+    df = dp.importData(args.filename)
     print("Columns: %i" % len(df.columns))
     print("Rows: %i" % df.shape[0])
-    #dp.showNaN(df)
+    print("checking missing data...")
+    dp.showNaN(df,args.verbose)
+    print("fixing missing data...")
+    df = dp.fixNaN(df,args.verbose,args.neighbours)
+    print("checking missing data...")
+    dp.showNaN(df,args.verbose)
 
 if __name__ == "__main__":
     main()
